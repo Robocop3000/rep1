70b41f899d54b62331f8628a1cda4aa2877a85dd
Robocop3000 <150103426+Robocop3000@users.noreply.github.com>
2024-09-19 18:49:56 -0600

    README.py

---
+++ README.py
@@ -0,0 +1,15 @@
+Esta es una calculadora básica en Python que realiza las siguientes operaciones:
+* Suma de dos números
+* Resta de dos números
+* Multiplicación de dos números
+* División de dos   
+ números
+* Suma de N números
+
+## Instrucciones de uso
+1. Clona este repositorio.
+2. Ejecuta el archivo `main.py` en tu terminal.
+3. Sigue las instrucciones en pantalla.
+
+## Licencia
+Este proyecto está bajo la licencia MIT. Para más detalles, consulta el archivo LICENSE.
